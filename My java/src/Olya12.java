7b28649567be129d32d71e32b30c19b304ac40fb
NShafarevich <120031216+NShafarevich@users.noreply.github.com>
2023-01-23 10:39:24 +0300

    Add files via upload

--- My java/src/Olya12.java
+++ My java/src/Olya12.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Olya12 {
+public class Olya12 {
+    public static void main(String[] args) {
+        int[] numbers = {10, 12, 14, 16, 18};
+        for (int k : numbers) {
+            System.out.print(k + " ");
+        }
+        System.out.println();
+        System.out.println();
+        for (int k : numbers) {
+            System.out.println(k);
+        }
+    }
 }
+
+
